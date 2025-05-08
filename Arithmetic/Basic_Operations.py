b9fdf27af6a9da4b844c1a335cace04d69465ab5
David <132290741+light-david@users.noreply.github.com>
2025-05-08 22:30:46 +0000

    Initial commit

---
+++ Arithmetic/Basic_Operations.py
@@ -0,0 +1,35 @@
+class basic_operations:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def addition(a, b, c):
+        """
+        This is a simple addition function
+        """
+        a + b = b + a
+        c = a + b
+        return c
+    
+    def subtraction(a, b, c):
+        """
+        This a simple subtraction function
+        """
+        c = a - b
+        return c
+    
+    def multiplication(a, b, c):
+        """
+        This is a simple multiplication function
+        """
+        a * b = b * a
+        c = a * b
+        return c
+    
+    def division(a, b, c):
+        """
+        This is a simple division function
+        """
+        c = a / b
+        return c
